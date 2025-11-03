058b6a14c3376aa326d99007db10602bf24601fe
Vishal Jain <jainvishal2212@gmail.com>
2025-11-03 18:11:45 +0000

    Initial Commit

---
+++ HoldApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HoldApp
+//
+//  Created by Vishal Jain on 03/11/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ HoldAppTests/HoldAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoldAppTests.swift
+//  HoldAppTests
+//
+//  Created by Vishal Jain on 03/11/2025.
+//
+
+import Testing
+@testable import HoldApp
+
+struct HoldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
